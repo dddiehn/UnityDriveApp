cbd04ce71ab82ccd8c25eb445454d9b889b491f0
Dominic Diehn <dddiehn@gmail.com>
2013-09-22 09:34:20 -0400

    intial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private void upload(ActionEvent event)
+    {
+        System.out.println("WHOA");
+    }
+    private void chooseFile(ActionEvent event)
+    {
+        System.out.println("WHOA");
+    }
+    private void upDirectory(ActionEvent event){
+
+    }
+    private void search(ActionEvent event){
+
+    }
+    private void clearSearch(ActionEvent event){
+
+    }
+    private void download(ActionEvent event){
+
+    }
+}
